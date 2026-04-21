3711544c57ea4cdea3ca947d17248fe587732f4d
Shubh Khandelwal <shubh4664@gmail.com>
2026-04-21 12:00:38 +0530

    Removed package-lock.json

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.use(express.static(__dirname));
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`App running on port ${PORT}`);
